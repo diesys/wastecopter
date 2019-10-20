cce679563cb559ec2e431727892468d7692a5c52
francescodg <francescodg4@gmail.com>
2019-10-20 11:50:50 +0200

    spawning garbage

--- server.py
+++ server.py
@@ -1,18 +1,31 @@
 from flask import Flask, request
 import json
 import os
-# from skimage.io import imread
-# from flask_uploads import *
+
+from skimage.io import imread
+from skimage.transform import resize
+from skimage.util import img_as_float
+from flask_uploads import *
+import tensorflow as tf
+import keras
+from keras.models import load_model
+from tensorflow.python.keras.backend import set_session
 
 app = Flask(__name__)
 
 # app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()
 
-# file = UploadSet('SecretKey', DATA)
 
-# photos = UploadSet('photos', IMAGES)
+photos = UploadSet('photos', IMAGES)
 
 # configure_uploads(app, photos)
+
+# tf_config = some_custom_config
+sess = tf.Session()
+graph = tf.get_default_graph()
+set_session(sess)
+
+model = load_model('static/simplenet.h5')
 
 @app.route("/")
 def home():
@@ -24,21 +37,30 @@ def save_session():
         json.dump(request.json, fp)
     return "", 200
 
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     print(request.files)
-#     if request.method == 'POST' and 'myimage' in request.files:
-#         filename = photos.save(request.files['myimage'])
-#         rec = Photo(filename=filename, user=g.user.id)
-
-#         print(type(rec))
+@app.route('/upload', methods=['POST'])
+def upload():
+    
+    if request.method == 'POST' and 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
         
-#         # rec.store()
-#         flash("Photo saved.")
-#         # return redirect(url_for('show', id=rec.id))
-#     return "ok", 200
+        I = imread(filename)
+        I = img_as_float(I[:, :, :-1])
+        I = resize(I, (128, 128))
+
+        global sess
+        global graph
+        with graph.as_default():
+            set_session(sess)
+            y = model.predict(I.reshape(1, 128, 128, 3))[0]
+
+            outstr = ""
+            for label, out in zip(["glass", "paper", "plastics", "metal", "trash"], y):
+                outstr += "{}, {:.2f}<br/>".format(label, out)
+            # outstr = "{} {} {}".format(I.shape[0], I.shape[1], y)
+
+        return outstr, 200
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, threaded=False)
 
